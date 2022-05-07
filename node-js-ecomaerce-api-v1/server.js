bee9ea3b6436f1e9c51a0a5b22778f104ec422be
mohelsadany <mohelsadany@gmail.com>
2022-05-07 09:37:49 +0200

    create new enhancements

--- node-js-ecomaerce-api-v1/server.js
+++ node-js-ecomaerce-api-v1/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-require('dotenv').config();
-const url = process.env.DATABASE_URL;
+dotenv.config({ path: 'config.env' });
 
-//  concat with database
+//  Concat with database
 mongoose
-  .connect(url)
+  .connect(process.env.DATABASE_URL)
   .then((conn) => {
     console.log(`database connected: ${conn.connection.host}`);
   })
@@ -16,15 +16,41 @@ mongoose
     process.exit(1);
   });
 
+// Express
 const app = express();
 
+// Middleware
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
   console.log(`mode:  ${process.env.NODE_ENV}`);
 }
 
+// Schema
+// 1- create schema
+const categorySchema = new mongoose.Schema({
+  name: String,
+});
+
+// 2- create model
+const CategoryModel = mongoose.model('Category', categorySchema);
+
+// Routes
+app.post('/', (req, res) => {
+  const name = req.body.name;
+  console.log('🚀 ~ file: server.js ~ line 40 ~ app.post ~ name', res.name);
+
+  const newCategory = new CategoryModel({ name: name });
+  newCategory
+    .save()
+    .then((doc) => {
+      res.json(doc);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 app.get('/', (req, res) => res.send('Hello World!'));
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`App run ${port}!`));
